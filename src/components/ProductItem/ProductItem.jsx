bc56afa8423de40dd2379447fe75690e95ad40df
akhilesh guleria <developakhilesh01@gmail.com>
2026-06-13 12:29:47 +0530

    made product item component card which show product in form of card

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../../store/CartSlice.js'
+import { Link } from 'react-router-dom'
 
 const ProductItem = ({product}) => {
-  
+  console.log(product)
+ const dispatch = useDispatch();
   return (
-    <div>
-      
-    </div>
+    <article  className="product-card">
+      <div className="product-card__image-wrap">
+      <img
+          alt={product.title}
+          className="product-card__image"
+          loading="lazy"
+          src={product.thumbnail}
+        />
+      </div>
+      <div className="product-card__body">
+        <h3 className="product-card__title">{product.title}</h3>
+        <p className="product-card__category">{product.category}</p>
+        <p className=''>{product.rating}/5</p>
+        <p className="product-card__price">Rs{product.price.toFixed(2)}</p>
+
+        <div className="product-card__actions">
+          <Link to={`product/${product.id}`}>View Product</Link>
+          <button
+            className="product-card__button product-card__button--solid"
+            onClick={() => dispatch(addToCart(product))}
+            type="button"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </article>
   )
+}
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default ProductItem
